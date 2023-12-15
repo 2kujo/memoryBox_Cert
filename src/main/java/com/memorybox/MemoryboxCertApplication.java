c9f5b73e277b1a3eb31031c6e8996179432e7887
kmj <rlaalswh531@gmail.com>
2023-12-15 17:27:29 +0900

    Initial commit

---
+++ src/main/java/com/memorybox/MemoryboxCertApplication.java
@@ -0,0 +1,13 @@
+package com.memorybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryboxCertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryboxCertApplication.class, args);
+    }
+
+}
