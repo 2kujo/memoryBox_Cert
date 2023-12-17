511a7110adaf4480ae57a2eab28dbed70a1d4ff9
김현교 <zkdlajt96@gmail.com>
2023-12-17 14:08:41 +0900

    :recycle: refactor: UserIdService로 UserId 초기화 로직 이동

--- src/main/java/com/memorybox/MemoryboxCertApplication.java
+++ src/main/java/com/memorybox/MemoryboxCertApplication.java
@@ -1,10 +1,7 @@
 package com.memorybox;
 
-import com.memorybox.service.UserIdService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MemoryboxCertApplication {
@@ -13,12 +10,4 @@ public class MemoryboxCertApplication {
         SpringApplication.run(MemoryboxCertApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner run(UserIdService userIdService) {
-        return (String[] args) -> {
-            for (int userId = 1; userId <= 10; userId++) {
-                userIdService.addUserId(userId);
-            }
-        };
-    }
 }
--- src/main/java/com/memorybox/service/UserIdService.java
+++ src/main/java/com/memorybox/service/UserIdService.java
@@ -1,5 +1,6 @@
 package com.memorybox.service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -7,6 +8,13 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 @Service
 public class UserIdService {
     private final ConcurrentLinkedDeque<Integer> userIdQueue = new ConcurrentLinkedDeque<>();
+
+    @PostConstruct
+    private void init() {
+        for (int userId = 1; userId <= 10; userId++) {
+            userIdQueue.offer(userId);
+        }
+    }
 
     public String getUserId() {
         Integer userId = userIdQueue.pop();
