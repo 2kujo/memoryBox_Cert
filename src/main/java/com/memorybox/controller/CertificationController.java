063d0b7a867503f2b1e58f19549c359447e43b3c
kmj <rlaalswh531@gmail.com>
2023-12-22 17:13:36 +0900

    🍋 modify : 쿠키 로직 제거

--- src/main/java/com/memorybox/controller/CertificationController.java
+++ src/main/java/com/memorybox/controller/CertificationController.java
@@ -18,38 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CertificationController {
 
     private final UserIdService userIdService;
-    private final UserIdCookieUtil cookieUtil;
 
     @GetMapping("/cert")
     public ResponseEntity<?> createCert() {
         log.info(" [REQUEST] createCert Call");
         String userId = userIdService.getUserId();
-        ResponseCookie userIdCookie = cookieUtil.makeUserIdCookie(userId);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, userIdCookie.toString())
                 .body(new UserIdResponseDto(userId));
-    }
-
-    @GetMapping("/special-cert")
-    public ResponseEntity<?> createSpecialCert(@CertUserId(required = false) long userId) {
-        ResponseCookie userIdCookie = null;
-        if (userId == 0L) {
-            String userIdString = userIdService.getUserId();
-            userIdCookie = cookieUtil.makeUserIdCookie(userIdString);
-            userId = Long.parseLong(userIdString);
-        }
-        ResponseCookie specialCookie = cookieUtil.makeSpecialCookie();
-        return makeResponseEntity(userId, specialCookie, userIdCookie);
-    }
-
-    private ResponseEntity<?> makeResponseEntity(long userId, ResponseCookie specialCookie, ResponseCookie userIdCookie) {
-        ResponseEntity.BodyBuilder builder = ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, specialCookie.toString());
-        if (userIdCookie != null) {
-            builder.header(HttpHeaders.SET_COOKIE, userIdCookie.toString());
-        }
-        return builder.body(new UserIdResponseDto(userId));
     }
 
 }
